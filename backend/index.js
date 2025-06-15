1acaaeecd9523c8b3b5145477d13133b89cd701f
aashii30 <aashi.sri30@gmail.com>
2025-06-16 00:46:45 +0530

    Fix login bugs

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
-const port=process.env.PORT || 5000
+const port=process.env.PORT || 8000
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
